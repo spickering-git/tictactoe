f7b1459d969dedd62e5dbde8435ab1db4733fa40
spickering-git <spickering@outlook.com>
2016-07-04 15:23:11 -0700

    test

--- src/config.js
+++ src/config.js
@@ -13,6 +13,7 @@ const config = {
   WEBHOOK_URL: process.env.WEBHOOK_URL,
   TICTACTOE_COMMAND_TOKEN: process.env.TICTACTOE_COMMAND_TOKEN,
   SLACK_TOKEN: process.env.SLACK_TOKEN,
+  SLACK_API_TOKEN: process.env.SLACK_API_TOKEN,
   ICON_EMOJI: ':stars:'
 }
 
