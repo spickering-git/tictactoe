ef2e2ae6821e7bd7eef578cb00b4df53c671c7fd
spickering-git <spickering@outlook.com>
2016-07-04 10:53:07 -0700

    typo

--- src/commands/start.js
+++ src/commands/start.js
@@ -41,7 +41,7 @@ const handler = (gameList, payload, res) => {
         {
           title: 'TicTacToe',
           color: '#2FA44F',
-          text: 'New game in ' + payload.channel_name + ' channel.',
+          text: 'New game in ' + payload.channel_name + ' channel.' +
           '\n' + currentGame.username1 + ' vs. ' +
           currentGame.username2,
           mrkdwn_in: ['text']
