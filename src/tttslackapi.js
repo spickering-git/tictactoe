b7abc1206ef663b5ea2eda2950ababcdd38e33c4
spickering-git <spickering@outlook.com>
2016-07-04 15:25:16 -0700

    test

--- src/tttslackapi.js
+++ src/tttslackapi.js
@@ -4,7 +4,7 @@
 const slack = require('slack')
 const config = require('./config')
 
-slack = new Slack(config('SLACK_TOKEN'));
+slack = new Slack(config('SLACK_API_TOKEN'));
 
 slack.api("users.list", function(err, response) {
     console.log(response);
