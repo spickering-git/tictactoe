20b70348cee722dd39b8cc3450fc802b7f102058
spickering-git <spickering@outlook.com>
2016-07-05 17:41:33 -0700

    test

--- src/tttslackapi.js
+++ src/tttslackapi.js
@@ -6,8 +6,10 @@ const config = require('./config')
 
 //slack = new Slack(config('SLACK_API_TOKEN'));
 slack.api.test({hello:'world'}, console.log);
-
-slack.auth.test(config('SLACK_API_TOKEN'), console.log);
+console.log(config('SLACK_API_TOKEN'));
+slack.auth.test(config('SLACK_API_TOKEN'), function(err,data){
+    console.log(data);
+});
 
 //slack.api("users.list", function(err, response) {
 //    console.log(response);
