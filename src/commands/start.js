6d852dcbaf2c80bfa40cf2c2e28d82748b96a289
spickering-git <spickering@outlook.com>
2016-07-05 21:32:56 -0700

    test

--- src/commands/start.js
+++ src/commands/start.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const config = require('../config');
 const game = require('../game');
 
-var slack = require('./tttslackapi');
+var slack = require('../tttslackapi');
 
 const msgDefaults = {
   response_type: 'in_channel',
